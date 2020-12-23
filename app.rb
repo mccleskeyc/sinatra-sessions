cb11fae0d636fc12ace40f6833bbed965213be2c
mccleskeyc <mccleskeyc@gmail.com>
2020-12-22 19:23:08 -0500

    commit tree c12132a56615cda6e2d1c8a168fc432ffb1945c4

--- app.rb
+++ app.rb
@@ -19,13 +19,14 @@ class App < Sinatra::Base
   end
 
   get '/set' do
-    # set the :foo key of the session hash equal to 'hello' here!
-    if session[:foo] == 'hello'
-      redirect '/fetch'
-    else
-      "Session value has not been set!"
-    end
+  # set the :foo key of the session hash equal to 'hello' here!
+  session[:foo] = "hello"
+  if session[:foo] == 'hello'
+    redirect '/fetch'
+  else
+    "Session value has not been set!"
   end
+end
 
   get '/fetch' do
     "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
@@ -36,10 +37,10 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
+    session[:id] = 1
     #set session id here
-
     if session[:id] == 1
-      # "Session ID set. It's currently set to #{session[:id]}."
+      # "Session ID set. It's currently set to #{session[:user_id]}."
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
@@ -51,7 +52,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    #clear session hash here
+    session = {}
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
